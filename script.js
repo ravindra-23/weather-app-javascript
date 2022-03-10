66ab9552fd4fb88ec084731375c0a0a145ec0fc0
Ravindra Patanwal <ravinderpatanwal@gmail.com>
2022-03-10 13:13:13 +0530

    fixed api link

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ const getTimeAndDay = () => {
 }
 
 const fetchCurrentWeather = async (city) => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
     const response = await fetch(url)
     const data = await response.json();
     console.log(data)
